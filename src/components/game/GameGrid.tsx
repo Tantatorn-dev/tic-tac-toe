dbd26b9634421db32539544cafae48ebe065deb5
Tantatorn <timefear007@gmail.com>
2023-03-04 13:43:10 +0700

    feat: add XO text

--- src/components/game/GameGrid.tsx
+++ src/components/game/GameGrid.tsx
@@ -1,25 +1,19 @@
 import { useState } from "react";
+import styles from "./GameGrid.module.css";
 
 const GameGrid = () => {
-  const [gameArr] = useState<string[]>([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
+  const [gameArr] = useState<string[]>(["", "", "", "", "", "", "", "", ""]);
 
   return (
     <div className="grid w-96 grid-cols-3 border border-purple-300">
-      {gameArr.map((_, index) => (
+      {gameArr.map((cell, index) => (
         <div
           key={index}
-          className="h-32 w-32 cursor-pointer border border-purple-400 hover:bg-indigo-900"
-        ></div>
+          className="flex h-32 w-32 cursor-pointer place-items-center justify-center border border-purple-400 hover:bg-indigo-900"
+        >
+          {cell === "X" && <p className={styles.xoText}>X</p>}
+          {cell === "O" && <p className={styles.xoText}>O</p>}
+        </div>
       ))}
     </div>
   );
